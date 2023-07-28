07e95e81bda09aa2d8df066a8c4b794d77909d0f
Ryan Stewart <ryanstewartalex@gmail.com>
2023-07-28 12:33:07 -0500

    Remove Ads In Middle And Bottom of Search Results

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,9 @@
 const tvcap = document.querySelector("#tvcap");
 const atvcap = document.querySelector("#atvcap");
+const tadsb = document.querySelector("#tadsb");
+const u3qv = document.querySelector(".U3A9Ac.qV8iec");
 
 if (tvcap) tvcap.remove();
 if (atvcap) atvcap.remove();
+if (tadsb) tadsb.remove();
+if (u3qv) u3qv.remove();
